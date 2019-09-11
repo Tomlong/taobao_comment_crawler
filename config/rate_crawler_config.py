b35f994ae788555264d187135ed1aca0ee249a58
terrylin <terrylin@emotibot.com>
2019-09-11 16:49:07 +0800

    Add rate_crawler

---
+++ config/rate_crawler_config.py
@@ -0,0 +1,82 @@
+import os
+import urllib
+
+VST_URL = 'https://login.taobao.com/member/vst.htm?st={}'
+MONGO_URI = os.environ.get('MONGODB', 'localhost:27017')
+PROXIES = {'http':'http:/122.117.138.228:80'}
+LOGIN_URL = 'https://login.taobao.com/member/login.html'
+LOGIN_HEADERS =  {
+    'Host':'login.taobao.com',
+    'User-Agent' : '5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+    'Referer' : 'https://login.taobao.com/member/login.html',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Connection' : 'Keep-Alive'
+}
+#用户名
+username = 'TomLong530'
+#ua字符串，taobao ua驗證碼
+ua = '120#bX1bSaY8FInVXSZciIYfAMvfbcLJuQqj0QTchmQ4yNpC906/BeM0EpJt3tpqR/WB//hMnOvZ2fGGYK0ZM2GGP0BYmuMMEfPmqZiul2iDBYligFNgN6DB4HvwABZsTDi9wDIasPJXpmxemWgVjK0lmM4SfthPFud+n74qcccF16NI2pBzBIX+w+Hc8l9A5MDX9rTESNuuNeh6iGmhLDqfeo3gZ8bgVCj5hi4F3+hCbpLyknLFlAPslfktp6pISIYz515/yvVUTn5PNO8BbvbcTaMPyEIbS5hpNee/PbbSjMgaSTvVCJIO7vxYg6ikMx38XNnUMrYNqH5GGd3dSucNjOq0JN60oQ9PhVo56tsufgRfIcitTBANW1MXXMNuIuK+y2TcI4voAmrc2wuRjyjjaQk3tA4kzI27v+1dhVLUgshxwz60lzL0MjQA4eqzBTpSy0C++GdO+iBDg/nl02KUYZ9Ig+duucrop5HzeQzfnamUO0fcgrpKjZXN8tXvuZCQeF37s/J2wV5+uj6aHJp+YtyTYz/H6j9SZHyYCx9ZyG/uNgVgFB84/NmymOtRmrynkiUfLIM9ROy+46GYyTnFH0e1kCcLWvW/BliVVaLTX5W4WSUUnuQdtrhT/YvDCIUunPRHCZ2unRxrHfkyVuVm/OaZAVmkdl0+I4Kt42GwlRkqx2er2t6InahgfCRo446xvL0ZFzUIL5dG15XoIK+5uy+mmBusLxG9'
+#密碼，透過taobao 加密
+password2 = '5035134d53410c1a6f9567c737f0d346e79982880cded92d59c18bdd2284bc6b5b8ff9d7d874759806f6b34b0442003ef697cbdc4f4b6e8bbacfc17a0b75aeea9c1cbb5a2f78f03943c6d37a375115a6e441e621b7267b478d82401586003b043980a992998c528e97d36405adeea03ea8ed0ab75017ce0a593a554a68692b65'
+post = {
+    'TPL_username': username,
+    'TPL_password_2': password2,
+    'ua': ua,
+    'TPL_redirect_url': 'http://i.taobao.com/my_taobao.htm?nekot=dG9tbG9uZzUzMA%3D%3D1567665966030',
+    'TPL_password': '',
+    'ncoSig': '',
+    'ncoSessionid': '',
+    'ncoToken': 'a3b8fcae9b63e74881eac8285fd8a5219f7909a4',
+    'slideCodeShow': False,
+    'useMobile': False,
+    'lang': 'zh_CN',
+    'loginsite': 0,
+    'newlogin': 0,
+    'TPL_redirect_url': '',
+    'from': 'tb',
+    'fc': 'default',
+    'style': 'default',
+    'css_style': '',
+    'keyLogin': False,
+    'qrLogin': True,
+    'newMini': False,
+    'newMini2': False,
+    'tid': '',
+    'loginType': 3,
+    'minititle': '',
+    'minipara': '',
+    'pstrong': '',
+    'sign': '',
+    'need_sign': '',
+    'isIgnore': '',
+    'full_redirect': '',
+    'sub_jump': '',
+    'popid': '',
+    'callback': '',
+    'guf': '',
+    'not_duplite_str': '',
+    'need_user_id': '',
+    'poy': '',
+    'gvfdcname': 10,
+    'gvfdcre': '',
+    'from_encoding': '',
+    'sub': '',
+    'loginASR': 1,
+    'loginASRSuc': 1,
+    'allp': '',
+    'oslanguage': 'zh-TW',
+    'sr': '1440*900',
+    'osVer': 'macos|10.136',
+    'naviVer': 'chrome|75.03770142',
+    'osACN': 'Mozilla',
+    'osAV': '5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+    'osPF': 'MacIntel',
+    'miserHardInfo': '',
+    'appkey': '00000000',
+    'nickLoginLink': '',
+    'mobileLoginLink': 'https://login.taobao.com/member/login.jhtml?useMobile=true',
+    'showAssistantLink': True,
+    'um_token': 'T7E1F6B7BDADF3AF0468FA984746D91447A94BE17BEC32A42B671C0253E',
+}
+#将POST的数据进行编码转换
+POST_DATA = urllib.parse.urlencode(post).encode('gbk')
---
+++ taobao_rate_crawler.py
@@ -0,0 +1,163 @@
+import re
+import time
+import urllib
+import gridfs
+import pymongo
+import logging
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, WebDriverException
+
+from rate_crawler_config import PROXIES, LOGIN_HEADERS, LOGIN_URL, POST_DATA, MONGO_URI, VST_URL
+
+logger = logging.getLogger(__name__)
+
+MAX_PAGE = 50
+session = requests.Session()
+session.proxies = PROXIES
+
+MONGO_CLIENT = pymongo.MongoClient(MONGO_URI)
+TAOBAO_URL_DB = MONGO_CLIENT['TAOBAO_URL']
+TAOBAO_RATE_PAGE_DB = MONGO_CLIENT['TAOBAO_RATE_PAGE']
+FS = gridfs.GridFS(TAOBAO_RATE_PAGE_DB)
+
+
+def get_st_code():
+    # 取得 st_code 的請取 url 
+    opener = urllib.request.build_opener()
+    request = urllib.request.Request(LOGIN_URL, POST_DATA, LOGIN_HEADERS)
+    response = opener.open(request)
+    content = response.read().decode('gbk')
+    pattern = re.compile('<script src=\"(.*)\"><\/script>')
+    match = pattern.search(content)
+    # 取得 st_code 請取 url
+    st_url = match.group(1)
+    # request st_code
+    st_response = session.get(st_url)
+    # parse st_code
+    st_match = re.search(r'"data":{"st":"(.*?)"}', st_response.text)
+    st_code = st_match.group(1)
+
+    return st_code
+
+
+def _crawl_rate_page(driver, job):
+    rate_page_db = TAOBAO_RATE_PAGE_DB['rate_page']
+    for page in range(MAX_PAGE):
+        logging.info('crawling page{}'.format(str(page+1)))
+        html = driver.page_source.encode('utf-8')
+        information = {
+            'product_title': job['product_title'],
+            'category': job['category'],
+            'item': job['item'],
+            'product_url': job['product_url'],
+            'page': page,
+        }
+
+        if not FS.find_one(information):
+            obj_id = FS.put(html, **information)
+            rate_page_db.insert({
+                'id': obj_id,
+                'product_title': job['product_title'],
+                'category': job['category'],
+                'item': job['item'],
+                'product_url': job['product_url'],
+                'page': page,
+                'status': 'ready',
+            })
+
+        try:
+            driver.find_element_by_css_selector('#J_Reviews > div > div.rate-page > div > a:last-child').click()
+            time.sleep(3)
+        except:
+            # 找不到下一頁
+            break
+
+def crawl_rate_page(driver, job):
+    timeout = 20
+    product_url = job['product_url']
+    try:
+        driver.get(product_url)
+        # 判斷是否有評論的Tab
+        WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.ID, "J_TabBarBox")))
+        time.sleep(2)
+    except TimeoutException:
+        logging.info('未找到評論Tab')
+        return
+
+    try:
+        # 頁面scroll至800位置，看到評論Tab，模擬人的動作
+        js = "window.scrollTo(0,800)"
+        driver.execute_script(js)
+        time.sleep(5)
+    except WebDriverException:
+        logging.info('頁面Scroll出問題')
+        return
+
+    try:
+        driver.find_element_by_xpath('//*[@id="J_TabBar"]/li[2]').click()
+        time.sleep(5)
+        logging.info('點擊累計評論Tab成功')
+    except:
+        logging.info('沒有累計評論的Tab')
+        return
+    
+    try:
+        driver.find_element_by_xpath('//*[@id="J_Reviews"]/div/div[@class="rate-grid"]')
+        _crawl_rate_page(driver, job)
+    except Exception as e:
+        logging.info(e)
+        logging.info('累計評論沒載入成功')
+        return
+
+
+def login():
+    # 透過取得 st_code 登入 taobao
+    st_code = get_st_code()
+    login_st_url = VST_URL.format(st_code)
+    # driver setup
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+
+    driver.implicitly_wait(10)
+    driver.get(login_st_url)
+
+    return driver
+
+def main():
+    try:
+        MONGO_CLIENT.server_info()
+        logging.info('MongoDB connect successfully')
+    except:
+        logging.info('MongoDB connect failed')
+        exit()
+    
+    # 登入
+    driver = login()
+    logging.info('login success')
+
+    # 爬取 rate page
+    product_urls_db = TAOBAO_URL_DB['product_url']
+    while(True):
+        job = product_urls_db.find_one_and_update({'status': 'ready'}, {'$set': {'status': 'crawling'}})
+        if job:
+            logging.info('crawling product title:{}'.format(job['product_title']))
+            crawl_rate_page(driver, job)
+            product_urls_db.find_one_and_update({'_id': job['_id']}, {'$set':{'status': 'finish'}})
+            logging.info('crawling finish')
+
+        else:
+            logging.info('Waiting for new product url')
+    
+        time.sleep(5)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
