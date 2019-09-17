a77f32ceb21b06bdfb3b8019924c27e8fbabc7a1
terrylin <terrylin@emotibot.com>
2019-09-17 14:39:08 +0800

    Use pyppeteer to crawl

--- taobao_rate_crawler.py
+++ taobao_rate_crawler.py
@@ -2,17 +2,14 @@ import re
 import time
 import urllib
 import gridfs
+import asyncio
 import pymongo
 import logging
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, WebDriverException
+from pyppeteer import launch
 
+from utils import slide_list
 from rate_crawler_config import PROXIES, LOGIN_HEADERS, LOGIN_URL, POST_DATA, MONGO_URI, VST_URL
 
 logger = logging.getLogger(__name__)
@@ -46,17 +43,18 @@ def get_st_code():
     return st_code
 
 
-def _crawl_rate_page(driver, job):
+async def _crawl_rate_page(page, job):
     rate_page_db = TAOBAO_RATE_PAGE_DB['rate_page']
-    for page in range(MAX_PAGE):
-        logging.info('crawling page{}'.format(str(page+1)))
-        html = driver.page_source.encode('utf-8')
+    for page_num in range(MAX_PAGE):
+        logging.info('crawling page{}'.format(str(page_num+1)))
+        page_content = await page.content()
+        html = page_content.encode('utf-8')
         information = {
             'product_title': job['product_title'],
             'category': job['category'],
             'item': job['item'],
             'product_url': job['product_url'],
-            'page': page,
+            'page': page_num,
         }
 
         if not FS.find_one(information):
@@ -67,96 +65,136 @@ def _crawl_rate_page(driver, job):
                 'category': job['category'],
                 'item': job['item'],
                 'product_url': job['product_url'],
-                'page': page,
+                'page': page_num,
                 'status': 'ready',
             })
 
         try:
-            swipe_down(driver, 1.2)
-            driver.find_element_by_css_selector('#J_Reviews > div > div.rate-page > div > a:last-child').click()
-            time.sleep(5)
+            next_page = await page.J('#J_Reviews > div > div.rate-page > div > a:last-child')
+            await next_page.click()
+            await asyncio.sleep(2)
         except:
-            # 找不到下一頁
+            logger.info('找不到下一頁')
             break
 
-def swipe_down(driver, second):
-    for i in range(int(second/0.1)):
-        js = "window.scrollBy(0,250)"
-        driver.execute_script(js)
-        time.sleep(0.2)
+async def check_pass_evaluate(page):
 
+    if len(page.frames) == 4:
+        logger.info('出現驗證')
+        frame_list = page.frames
+        evaluate_frame = frame_list[3]
+        await evaluate_frame.Jeval('#nocaptcha', 'node => node.style')
+        await evaluate_frame.hover('#nc_1_n1z')
+        
+        logger.info('開始破解驗證')
+        await page.mouse.down()
+        slides = slide_list(260)
+        logger.info(f'{slides}')
+        x = page.mouse._x
+        for distance in slides:
+            x += distance
+            await page.mouse.move(x, 0, )
+        await page.mouse.up()
+        
+        logger.info('完成破解驗證')
+        await asyncio.sleep(5)
+        logger.info(f'{page.frames}')
+        await page.reload()
+        await asyncio.sleep(5)
+        logger.info('重新讀取')
+        await click_rate(page)
+    else:
+        logger.info('未出現驗證')
 
-def crawl_rate_page(driver, job):
-    timeout = 20
+async def click_rate(page):
+    timeout = 10
+    try:
+        await asyncio.sleep(5)
+        # 按累計評論
+        comment_button = await page.Jx('//*[@id="J_TabBar"]/li[2]')
+        logging.info(f'按累計評論{comment_button}')
+        await comment_button[0].click()
+        await asyncio.sleep(3)
+    except Exception as e:
+        logging.info(f'按累計評論Tab失敗{e}')
+        return
+
+async def crawl_rate_page(page, job):
+    timeout = 10
     product_url = job['product_url']
-    try:
-        driver.get(product_url)
-        # 判斷是否有評論的Tab
-        WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.ID, "J_TabBarBox")))
-        time.sleep(2)
-    except TimeoutException:
-        logging.info('未找到評論Tab')
-        return
-
-    try:
-        # 頁面scrol，看到評論Tab，模擬人的動作
-        swipe_down(driver, 0.3)
+    await page.goto(product_url)
+    await click_rate(page)
     
-    except WebDriverException:
-        logging.info('頁面Scroll出問題')
-        return
-
+    # 等待是否有驗證frame
+    await asyncio.sleep(5)
     try:
-        driver.find_element_by_xpath('//*[@id="J_TabBar"]/li[2]').click()
-        time.sleep(5)
-        logging.info('點擊累計評論Tab成功')
-    except:
-        logging.info('沒有累計評論的Tab')
-        return
-    
-    
-    try:
-        iframe_list = driver.find_elements_by_tag_name('iframe')
-        # 出現滑動驗證
-        if len(iframe_list) == 3:
-            driver.switch_to_frame(iframe_list[-1])
-        WebDriverWait(driver, 10, 0.5).until(EC.presence_of_element_located((By.ID, "nc_1_n1z"))) #等待滑动拖动控件出现
-        swipe_button = driver.find_element_by_id('nc_1_n1z') #获取滑动拖动控件
-
-        #模拟拽托
-        action = ActionChains(driver) # 实例化一个action对象
-        action.click_and_hold(swipe_button).perform() # perform()用来执行ActionChains中存储的行为
-        action.reset_actions()
-        action.move_by_offset(580, 0).perform() # 移动滑块
+        await check_pass_evaluate(page)
 
     except Exception as e:
-        print ('get button failed: ', e)
+        print ('驗證失敗 ', e)
     
     try:
-        driver.find_element_by_xpath('//*[@id="J_Reviews"]/div/div[@class="rate-grid"]')
-        _crawl_rate_page(driver, job)
+        rate_part = await page.Jx('//*[@id="J_Reviews"]/div/div[@class="rate-grid"]')
+        if len(rate_part) == 1:
+            await _crawl_rate_page(page, job)
     except Exception as e:
         logging.info(e)
         logging.info('累計評論沒載入成功')
         return
 
 
-def login():
+async def login():
     # 透過取得 st_code 登入 taobao
     st_code = get_st_code()
     login_st_url = VST_URL.format(st_code)
+
     # driver setup
-    chrome_options = webdriver.ChromeOptions()
+    launch_kwargs = {
+        "headless": True,
+        'args': [
+            '--no-sandbox',
+        ],
+    }
+
+    browser = await launch(launch_kwargs)
+    page = await browser.newPage()
+    await page.goto(login_st_url)
+
+    '''chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_experimental_option('excludeSwitches', ['enable-automation']) 
-    driver = webdriver.Chrome('./chromedriver', options = chrome_options)
+    driver = webdriver.Chrome(options = chrome_options)
 
     driver.implicitly_wait(10)
-    driver.get(login_st_url)
+    driver.get(login_st_url)'''
 
-    return driver
+    return page
+
+
+async def start():
+     # 登入
+    page = await login()
+    logging.info('login success')
+    await asyncio.sleep(5)
+
+    # 爬取 rate page
+    product_urls_db = TAOBAO_URL_DB['product_url']
+
+
+    while(True):
+        job = product_urls_db.find_one_and_update({'status': 'ready'}, {'$set': {'status': 'crawling'}})
+        if job:
+            logging.info('crawling product title:{}'.format(job['product_title']))
+            await crawl_rate_page(page, job)
+            product_urls_db.find_one_and_update({'_id': job['_id']}, {'$set':{'status': 'finish'}})
+            logging.info('crawling finish')
+
+        else:
+            logging.info('Waiting for new product url')
+    
+        await asyncio.sleep(10)
 
 def main():
     try:
@@ -166,25 +204,9 @@ def main():
         logging.info('MongoDB connect failed')
         exit()
     
-    # 登入
-    driver = login()
-    logging.info('login success')
-    time.sleep(5)
-
-    # 爬取 rate page
-    product_urls_db = TAOBAO_URL_DB['product_url']
-    while(True):
-        job = product_urls_db.find_one_and_update({'status': 'ready'}, {'$set': {'status': 'crawling'}})
-        if job:
-            logging.info('crawling product title:{}'.format(job['product_title']))
-            crawl_rate_page(driver, job)
-            product_urls_db.find_one_and_update({'_id': job['_id']}, {'$set':{'status': 'finish'}})
-            logging.info('crawling finish')
-
-        else:
-            logging.info('Waiting for new product url')
-    
-        time.sleep(10)
+    loop = asyncio.get_event_loop()
+    task = asyncio.ensure_future(start())
+    loop.run_until_complete(task)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
